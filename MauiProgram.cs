78fd1c49d57a3b3439b818e88fef4cd2d1c18a6b
Veronika Vanitseva <Veronika.Vanitseva@tthkee.onmicrosoft.com>
2025-11-08 13:16:35 +0200

    dark light theme upd

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -4,6 +4,12 @@ using SimonApp1.Database;
 using SimonApp1.Services;
 using SimonApp1.Views;
 
+#if ANDROID
+using Microsoft.Maui.Handlers;
+using Android.Content.Res;
+using AColor = Android.Graphics.Color;
+#endif
+
 namespace SimonApp1;
 
 public static class MauiProgram
@@ -21,15 +27,35 @@ public static class MauiProgram
         // ✅ Регистрируем сервисы
         builder.Services.AddSingleton<SettingsService>();
         builder.Services.AddSingleton<ThemeService>();
-        builder.Services.AddSingleton<LanguageService>(); // ← заменили LocalizationService на LanguageService
-
-        // ✅ Регистрируем базу данных
+        builder.Services.AddSingleton<LanguageService>();
         builder.Services.AddSingleton<AppDatabase>();
 
         // ✅ Регистрируем страницы
         builder.Services.AddTransient<WelcomePage>();
         builder.Services.AddTransient<MainPage>();
         builder.Services.AddTransient<SettingsPage>();
+
+#if ANDROID
+        // ✅ Убираем подчеркивание (underline) в Entry
+        EntryHandler.Mapper.AppendToMapping("RemoveUnderline", (handler, view) =>
+        {
+            if (handler.PlatformView != null)
+            {
+                handler.PlatformView.BackgroundTintList =
+                    ColorStateList.ValueOf(AColor.Transparent);
+            }
+        });
+
+        // ✅ Убираем подчеркивание (underline) в Picker
+        PickerHandler.Mapper.AppendToMapping("RemoveUnderline", (handler, view) =>
+        {
+            if (handler.PlatformView != null)
+            {
+                handler.PlatformView.BackgroundTintList =
+                    ColorStateList.ValueOf(AColor.Transparent);
+            }
+        });
+#endif
 
         return builder.Build();
     }
